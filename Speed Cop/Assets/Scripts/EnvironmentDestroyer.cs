7b4ae16d4de64696b4a44d7730c2b07a622557a4
FridgeFace16 <kylemakes98@gmail.com>
2019-05-07 16:26:10 +1000

    First Version of GDD, First workings on prototype project.

---
+++ Speed Cop/Assets/Scripts/EnvironmentDestroyer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentDestroyer : MonoBehaviour {
+
+    Collider2D ObjectDestroyCollider;
+    Collider2D[] OverlapResult;
+    ContactFilter2D contactFilter;
+	// Use this for initialization
+	void Start () {
+
+        ObjectDestroyCollider = GetComponent<Collider2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ObjectDestroyCollider.OverlapCollider(contactFilter, OverlapResult)
+
+
+        if(OverlapResult.Length > 0)
+        {
+             Destroy(OverlapResult.GetValue(0));
+        }
+	}
+}
---
+++ Speed Cop/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    Rigidbody2D rb;
+    Vector2 targetPos;
+
+
+    public float Yincriment;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+        // Initialise to be the same as current position, awaiting new value from input
+        targetPos = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // maxDistanceDelta in MoveTowards() becomes speed because it limits the rate at which input can incriment position towards targetpos
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincriment);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincriment);
+        }
+
+
+
+    }
+}
